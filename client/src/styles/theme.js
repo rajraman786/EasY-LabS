8d1199affe7af55db968fe0c81e2eb0a98837e12
deepdarshan21 <deepdarshan21@gmail.com>
2022-05-02 00:07:00 +0530

    theme add

---
+++ client/src/styles/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#049C53",
+        },
+    },
+});
+
+export default theme;
