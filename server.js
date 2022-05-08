f239a44e98b7d6c5e9cf85cb72b0bd88f241c5ee
deepdarshan21 <deepdarshan21@gmail.com>
2022-05-08 12:23:13 +0530

    Server Setup

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+const path = require("path");
 const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cors());
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
 
 app.get("/", (req, res) => {
     res.send("Up and running");
@@ -10,4 +21,4 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 4509;
 
-app.listen(PORT, console.log(`Server is up and running at port ${PORT}`))
+app.listen(PORT, console.log(`Server is up and running at port ${PORT}`));
