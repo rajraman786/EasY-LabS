79b8ba6e46858dee0ab31d8adf39d9e1b56766aa
deepdarshan21 <deepdarshan21@gmail.com>
2022-05-09 14:40:21 +0530

    Login route complete

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,14 @@
 const UserDB = require("../models/Users");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+require("dotenv").config("../.env");
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 // @desc    Allow users to sign up
 // route    POST /auth/signup
 // @access  Public
-
 exports.signup = (req, res, next) => {
     const { name, email, entryNo, password, role } = req.body;
 
@@ -52,4 +56,69 @@ exports.signup = (req, res, next) => {
                 error: err,
             });
         });
+};
+
+// @desc    Allow users to sign in
+// route    POST /auth/login
+// @access  Public
+exports.login = (req, res, next) => {
+    const { email, entryNo, password } = req.body;
+
+    if (email) {
+        UserDB.findOne({ email }).then(async (user) => {
+            if (user) {
+                if (await bcrypt.compare(password, user.password)) {
+                    return res.status(200).json({
+                        success: true,
+                        successMessage: "Successfully Logged-in",
+                        token: jwt.sign(
+                            {
+                                id: user._id,
+                                email: user.email,
+                            },
+                            JWT_SECRET
+                        ),
+                    });
+                } else {
+                    return res.status(404).json({
+                        success: false,
+                        errorMessage: "Incorrect Email/Password",
+                    });
+                }
+            } else {
+                return res.status(404).json({
+                    success: false,
+                    errorMessage: "Incorrect Email/Password",
+                });
+            }
+        });
+    } else if (entryNo) {
+        UserDB.findOne({ entryNo }).then(async (user) => {
+            if (user) {
+                if (await bcrypt.compare(password, user.password)) {
+                    return res.send(200).json({
+                        success: true,
+                        successMessage: "Successfully logged in into account",
+                        token: jwt.sign(
+                            {
+                                id: user._id,
+                                email: user.email,
+                            },
+                            JWT_SECRET
+                        ),
+                    });
+                } else {
+                    return res.status(404).json({
+                        success: false,
+                        errorMessage: "Incorrect Entry Number/Password",
+                    });
+                }
+            } else {
+                return res.status(404).json({
+                    success: false,
+                    errorMessage: "Incorrect Entry Number/Password",
+                });
+            }
+        });
+    }
 };
