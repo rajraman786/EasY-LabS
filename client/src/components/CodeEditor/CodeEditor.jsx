38f967f5abc654fc6f7bc5c5305ba95404a2ba0b
deepdarshan21 <deepdarshan21@gmail.com>
2022-05-30 04:31:21 +0530

    Codemirror implement

--- client/src/components/CodeEditor/CodeEditor.jsx
+++ client/src/components/CodeEditor/CodeEditor.jsx
@@ -2,18 +2,26 @@ import { useEffect } from "react";
 import Codemirror from "codemirror";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/dracula.css";
-import "codemirror/mode/javascript/javascript";
+// import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/python/python";
 import "codemirror/addon/edit/closetag";
 import "codemirror/addon/edit/closebrackets";
 import "./CodeEditor.css";
 
 const CodeEditor = ({ props }) => {
+    const language = {
+        python: {
+            name: "python",
+            version: 3,
+            // singleLineStringErrors: false,
+        },
+    };
     useEffect(() => {
-        const init = async () => {
-            await Codemirror.fromTextArea(document.getElementById("code-editor"), {
-                mode: { name: "javascript", json: true },
+        const init =  () => {
+            Codemirror.fromTextArea(document.getElementById("editor"), {
+                mode: language.python,
                 theme: "dracula",
-                autoCloseTags: true,
+                indentUnit: 4,
                 autoCloseBrackets: true,
                 lineNumbers: true,
             });
@@ -22,7 +30,11 @@ const CodeEditor = ({ props }) => {
         init();
     }, []);
 
-    return <textarea id="code-editor"></textarea>;
+    return (
+        <>
+            <textarea id="editor"></textarea>
+        </>
+    );
 };
 
 export default CodeEditor;
