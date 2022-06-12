7a10d5de6d610e419318055253cbec9c026d2b22
deepdarshan21 <deepdarshan21@gmail.com>
2022-06-12 16:06:40 +0530

    CodeEditor Page basic layout

--- client/src/components/CodeEditor/CodeEditor.jsx
+++ client/src/components/CodeEditor/CodeEditor.jsx
@@ -1,40 +1,74 @@
-import { useEffect } from "react";
-import Codemirror from "codemirror";
+import { useState } from "react";
 import "codemirror/lib/codemirror.css";
-import "codemirror/theme/dracula.css";
-// import "codemirror/mode/javascript/javascript";
-import "codemirror/mode/python/python";
-import "codemirror/addon/edit/closetag";
-import "codemirror/addon/edit/closebrackets";
-import "./CodeEditor.css";
 
-const CodeEditor = ({ props }) => {
-    const language = {
+import "codemirror/theme/dracula.css";
+import "codemirror/theme/material.css";
+import "codemirror/theme/mdn-like.css";
+import "codemirror/theme/the-matrix.css";
+import "codemirror/theme/night.css";
+
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
+import "codemirror/mode/python/python";
+
+import "codemirror/addon/edit/closebrackets";
+import "codemirror/addon/edit/closetag";
+
+import "./CodeEditor.css";
+import { Controlled as ControlledEditorComponent } from "react-codemirror2";
+
+const Editor = ({ language, value, setEditorState }) => {
+    const [theme, setTheme] = useState("dracula");
+    const [val, setVal] = useState("");
+    const lan = {
         python: {
             name: "python",
             version: 3,
             // singleLineStringErrors: false,
         },
     };
-    useEffect(() => {
-        const init =  () => {
-            Codemirror.fromTextArea(document.getElementById("editor"), {
-                mode: language.python,
-                theme: "dracula",
-                indentUnit: 4,
-                autoCloseBrackets: true,
-                lineNumbers: true,
-            });
-        };
+    const handleChange = (editor, data, value) => {
+        // setEditorState(value);
+        setVal(value);
+    };
 
-        init();
-    }, []);
+    const themeArray = ["material", "mdn-like", "the-matrix", "night", "dracula"];
+    // const themeArray = [];
 
     return (
-        <>
-            <textarea id="editor"></textarea>
-        </>
+        <div className="editor-container">
+            <div style={{ marginBottom: "10px" }}>
+                <label for="cars">Choose a theme: </label>
+                <select
+                    name="theme"
+                    onChange={(el) => {
+                        setTheme(el.target.value);
+                    }}
+                >
+                    {themeArray.map((theme) => (
+                        <option value={theme}>{theme}</option>
+                    ))}
+                </select>
+            </div>
+            <ControlledEditorComponent
+                onBeforeChange={handleChange}
+                // value={value}
+                value={val}
+                className="code-mirror-wrapper"
+                options={{
+                    lineWrapping: true,
+                    lint: true,
+                    // mode: language,
+                    mode: lan.python,
+                    lineNumbers: true,
+                    theme: theme,
+                    autoCloseTags: true,
+                    autoCloseBrackets: true,
+                }}
+            />
+        </div>
     );
 };
 
-export default CodeEditor;
+export default Editor;
