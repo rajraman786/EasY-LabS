d25a617d71853a9046ce09dc041062d112cd84f0
deepdarshan21 <deepdarshan21@gmail.com>
2022-06-17 15:52:00 +0530

    Enabled the option for input in compiler

--- controllers/compiler.js
+++ controllers/compiler.js
@@ -5,18 +5,14 @@ const axios = require("axios");
 // route    POST /compiler
 // @access  Public
 exports.compiler = async (req, res, next) => {
-    const { code, lang } = req.body;
-    console.log(code);
-    console.log(lang);
-    // res.status(200).json({
-    //     message: "running atleast",
-    // });
+    const { code, input, lang, langVersion } = req.body;
 
     try {
         const response = await axios.post("https://api.jdoodle.com/v1/execute", {
             script: code,
+            stdin: input,
             language: lang,
-            versionIndex: 4,
+            versionIndex: langVersion,
             clientId: "bc5828aa33ac8af193bf83fa69c47172",
             clientSecret: "2136a46bf4f1df005edf6bc5ba567d784448c9e4165116bb8f4851c23a3f706d",
         });
