bbbdfc68f556ab0296597a6d38739b4621860df9
deepdarshan21 <deepdarshan21@gmail.com>
2022-06-19 14:14:16 +0530

    Hide the secret information

--- controllers/compiler.js
+++ controllers/compiler.js
@@ -1,11 +1,14 @@
 require("dotenv").config("../.env");
 const axios = require("axios");
 
+require("dotenv").config("../.env");
+
 // @desc    Compiler
 // route    POST /compiler
 // @access  Public
 exports.compiler = async (req, res, next) => {
     const { code, input, lang, langVersion } = req.body;
+    console.log(code, input, lang, langVersion);
 
     try {
         const response = await axios.post("https://api.jdoodle.com/v1/execute", {
@@ -13,8 +16,8 @@ exports.compiler = async (req, res, next) => {
             stdin: input,
             language: lang,
             versionIndex: langVersion,
-            clientId: "bc5828aa33ac8af193bf83fa69c47172",
-            clientSecret: "2136a46bf4f1df005edf6bc5ba567d784448c9e4165116bb8f4851c23a3f706d",
+            clientId: process.env.JDOODLE_CLIENTID,
+            clientSecret: process.env.JDOODLE_CLIENTSECRET,
         });
         res.status(200).json(response.data);
     } catch (err) {
